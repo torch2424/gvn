35890de1d39958ed4ac05819c5d1a0094185d44b
Aaron Turner <aaron@aaronthedev.com>
2018-01-04 00:19:48 -0800

    Made gvn into an executable script

--- README.md
+++ README.md
@@ -12,5 +12,5 @@ Quick Copy-pastable way to prefer remote gvn, over local:
 ```
 # Import of gvn.sh
 # https://github.com/torch2424/gvn
-GVN_PATH="$HOME/.files_libs/gvn.sh"; GVN_URL="https://raw.githubusercontent.com/torch2424/gvn/master/gvn.sh"; if [ -f "$GVN_PATH" ]; then source "$GVN_PATH"; elif curl -o /dev/null -sIf "$GVN_URL"; then curl -s "$GVN_URL" -o /tmp/gvn.sh; source /tmp/gvn.sh; else echo "Could not load gvn from $GVN_PATH or $GVN_URL. Please check your gvn path and internet connection. Halting execution of the script..."; exit 1; fi;
+GVN_PATH="$HOME/.files_libs/gvn/gvn.local.sh"; GVN_URL="https://raw.githubusercontent.com/torch2424/gvn/master/gvn.sh"; if curl -o /dev/null -sIf "$GVN_URL"; then curl -s "$GVN_URL" -o /tmp/gvn.sh; source /tmp/gvn.sh; elif [ -f "$GVN_PATH" ]; then source "$GVN_PATH"; else echo "Could not load gvn from $GVN_PATH or $GVN_URL. Please check your gvn path and internet connection. Halting execution of the script..."; exit 1; fi;
 ```
--- gvn.sh
+++ gvn.sh
@@ -63,73 +63,61 @@ function printgvninfo() {
   echo "Try: https://github.com/garethrees/git-to-svn-guide/blob/master/README.md"
 }
 
+
 # CLI For git users to do svn stuff
 # USing this and learning repo as practice: https://github.com/garethrees/git-to-svn-guide/blob/master/README.md
-function gvn() {
+if [ "$#" -lt 1 ]; then
+  printgvninfo
+elif [ "$1" == "help" ]; then
+  printgvninfo
+elif [ "$1" == "clone" ]; then
+  # Git clone -> svn checkout
+  svn checkout "$2"
+elif [ "$1" == "pull" ]; then
+  # git pull -> svn update
+  svn update
+elif [ "$1" == "add" ]; then
+  # git add -> svn changelist my-changelist [File here]
+  svn add --force "$2"
+  svn changelist "$GVN_CHANGELIST" "$2"
+elif [ "$1" == "reset" ]; then
+  # git reset -> svn changelist --remove [File here]
+  svn changelist --remove "$2"
+elif [ "$1" == "status" ]; then
+  # git status -> svn status
+  # Doing both info and stuts, because git will show you current branch and stuff
+  svn info
+  echo " "
+  echo "SVN Status:"
+  echo " "
+  svn status | grep -v gvn
+  echo " "
+  echo "GVN Changes to be committed:"
+  echo "    (use \"gvn reset <file>\" to unstage)"
+  svn st --changelist "$GVN_CHANGELIST" | grep -v Changelist | cut -b 3-
+  echo " "
+elif [ "$1" == "checkout" ]; then
+  # git checkout file -> svn checkout file
+  echo "This is checkout for files..."
+  svn revert "$2"
+elif [ "$1" == "commit" ]; then
 
-  if [ "$#" -lt 1 ]; then
-    printgvninfo
-  elif [ "$1" == "help" ]; then
-    printgvninfo
-  elif [ "$1" == "clone" ]; then
-    # Git clone -> svn checkout
-    svn checkout "$2"
-  elif [ "$1" == "pull" ]; then
-    # git pull -> svn update
-    svn update
-  elif [ "$1" == "add" ]; then
-    # git add -> svn changelist my-changelist [File here]
-    svn add --force "$2"
-    svn changelist "$GVN_CHANGELIST" "$2"
-  elif [ "$1" == "reset" ]; then
-    # git reset -> svn changelist --remove [File here]
-    svn changelist --remove "$2"
-  elif [ "$1" == "status" ]; then
-    # git status -> svn status
-    # Doing both info and stuts, because git will show you current branch and stuff
-    svn info
-    echo " "
-    echo "SVN Status:"
-    echo " "
-    svn status | grep -v gvn
-    echo " "
-    echo "GVN Changes to be committed:"
-    echo "    (use \"gvn reset <file>\" to unstage)"
-    svn st --changelist "$GVN_CHANGELIST" | grep -v Changelist | cut -b 3-
-    echo " "
-  elif [ "$1" == "checkout" ]; then
-    # git checkout file -> svn checkout file
-    echo "This is checkout for files..."
-    svn revert "$2"
-  elif [ "$1" == "commit" ]; then
-
-    if [ "$#" -eq 2 ]; then
-    # git add $1 && git commit -m "$2" && git push origin HEAD -> svn commit $2 -m "$3"
-    # After &&, # Remove all files from changelist, but keep changelist
-    echo "Commiting changelist..."
-    svn commit --changelist "$GVN_CHANGELIST" --keep-changelists -m "$2" && \
-    svn changelist --remove --recursive --cl "$GVN_CHANGELIST" . | grep -v D
-    else
-      echo "Must enter a commit message to commit the current changelist."
-    fi
-  elif [ "$1" == "log" ]; then
-    # git log -n 10 -> svn log -v -l10
-    svn log -l 5 -r PREV:HEAD
-  elif [ "$1" == "diff" ]; then
-    # git diff [FILE]. If git supports single file diffing?
-    echo "Using Vim For colorful diffs"
-    svn diff -r HEAD "$2" | vim -R -
+  if [ "$#" -eq 2 ]; then
+  # git add $1 && git commit -m "$2" && git push origin HEAD -> svn commit $2 -m "$3"
+  # After &&, # Remove all files from changelist, but keep changelist
+  echo "Commiting changelist..."
+  svn commit --changelist "$GVN_CHANGELIST" --keep-changelists -m "$2" && \
+  svn changelist --remove --recursive --cl "$GVN_CHANGELIST" . | grep -v D
   else
-    echo "Command $1 is not suported. Type [gvn help] for info/help/usage and tips!"
+    echo "Must enter a commit message to commit the current changelist."
   fi
-}
-
-# Some gvn aliases
-alias gvnh="gvn help"
-alias gvna="gvn add"
-alias gvnr="gvn reset"
-alias gvnp="gvn pull"
-alias gvns="gvn status"
-alias gvnc="gvn commit"
-alias gvnl="gvn log"
-alias gvnd="gvn diff"
+elif [ "$1" == "log" ]; then
+  # git log -n 10 -> svn log -v -l10
+  svn log -l 5 -r PREV:HEAD
+elif [ "$1" == "diff" ]; then
+  # git diff [FILE]. If git supports single file diffing?
+  echo "Using Vim For colorful diffs"
+  svn diff -r HEAD "$2" | vim -R -
+else
+  echo "Command $1 is not suported. Type [gvn help] for info/help/usage and tips!"
+fi
